54cdfbfe2231ec4799e59553d22570155949ea8a
kidcompassion <sallypoulsen@gmail.com>
2016-06-11 11:40:01 -0600

    Adds loading for existing fields on page load.

--- js/redactyl.js
+++ js/redactyl.js
@@ -3,6 +3,9 @@ jQuery(document).ready(function(){
 //need to add and delete fields appropriately
 //need to ensure create fields remain on the page on reload.
 
+
+jQuery();
+
 //alert(bling);
 counter = 1;
 
@@ -10,7 +13,7 @@ counter = 1;
 	newField = "<tr><td></td><td><div class = field ><input type='text' name='redactyl_settings[redactyl_text_field_1]' value = "+bling+">";
 	newField += "<a class='delete' href='#'><span class='dashicons dashicons-dismiss'></span></a></div></td></tr>";
 
-
+jQuery(newField).insertAfter('tr:last-child');
 jQuery('.form-table').append('<a class="add" href="#">Add another word</a>');
 
 
