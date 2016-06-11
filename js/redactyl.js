7289d5651b8828844d0ca4a8347167273b881497
kidcompassion <sallypoulsen@gmail.com>
2016-06-11 11:32:49 -0600

    Gets admin field working as expected.

--- js/redactyl.js
+++ js/redactyl.js
@@ -1,40 +1,48 @@
 jQuery(document).ready(function(){
 
+//need to add and delete fields appropriately
+//need to ensure create fields remain on the page on reload.
 
+//alert(bling);
 counter = 1;
 
 	var newField;
-	newField = "<tr><td><div class = field ><input type='text' name='redactyl_settings[redactyl_text_field_1]'>";
-	newField += "<a class='add' href='#'>+</a>";
-	newField += "<a class='delete' href='#'>-</a></div></td></tr>";
+	newField = "<tr><td></td><td><div class = field ><input type='text' name='redactyl_settings[redactyl_text_field_1]' value = "+bling+">";
+	newField += "<a class='delete' href='#'><span class='dashicons dashicons-dismiss'></span></a></div></td></tr>";
 
 
-	//jQuery(newField).insertAfter('tr:first-child');
+jQuery('.form-table').append('<a class="add" href="#">Add another word</a>');
 
+
+	//Add new fields
 	jQuery('.add').bind('click', function(e){
-
-	e.stopPropagation;
-	addCopy();
-	jQuery('.field').each(function(key, value){
-		jQuery(this).addClass('field-'+ key);
+		e.stopPropagation;
+		addCopy();
+		//console.log('bling');
+		jQuery('.field').each(function(key, value){
+			jQuery(this).addClass('field-'+ key);
+		});
+		jQuery('.delete').bind('click', function(e){
+			e.stopPropagation;
+			jQuery(this).closest('tr').remove();
+			console.log(jQuery(this));
+		});
 	});
 
-	jQuery('.delete').bind('click', function(e){
-		jQuery(this).parent().remove();
-	});
-
-});
+	//remove field
 
 
 
 
-function addCopy(counter){
-	
-	var closestRow = jQuery('.add').closest('tr');
+
+
+
+function addCopy(){
+	//Get the current parent tr
+	var closestRow = jQuery('tr').last();
+	//Insert the new field after
 	jQuery(newField).insertAfter(closestRow);
-
-
-
+	console.log(closestRow);
 }
 
 
