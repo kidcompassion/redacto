942773cce0680b76d83e09f68d3f52f48050cfdd
kidcompassion <sallypoulsen@gmail.com>
2016-06-11 13:56:00 -0600

    dynamically generates id numbers for input fields.

--- js/redactyl.js
+++ js/redactyl.js
@@ -1,24 +1,51 @@
 jQuery(document).ready(function(){
+		
+var newField;	
+generate_unique_integer(newField);
 
-//need to add and delete fields appropriately
-//need to ensure create fields remain on the page on reload.
+
+function generate_unique_integer(){	
+	length = jQuery('.form-table input[type="text"]').length+1;
+	console.log(length);
+	for(i = 0; i<length; i++){
+
+		newField = "<tr><td></td><td><input type='text' name='redactyl_settings[redactyl_text_field_"+ i +"]' value = "+bling+">";
+		newField += "<a class='delete' href='#'><span class='dashicons dashicons-dismiss'></span></a></td></tr>";
+		console.log(i);
+	}
+
+}
+
+//	i=jQuery('.form-table input[type="text"]').length;	
 
 
-jQuery();
+
 
 //alert(bling);
-counter = 1;
 
-	var newField;
-	newField = "<tr><td></td><td><div class = field ><input type='text' name='redactyl_settings[redactyl_text_field_1]' value = "+bling+">";
-	newField += "<a class='delete' href='#'><span class='dashicons dashicons-dismiss'></span></a></div></td></tr>";
 
-jQuery(newField).insertAfter('tr:last-child');
-jQuery('.form-table').append('<a class="add" href="#">Add another word</a>');
+
+console.log();
+
+
+//Increment the number in the name value on the fields so i can pass it to the database
+
+//So the problem is that i is always overwritten by its global value
+
+//on load, the loaded fields should already increment
+
+	
+generate_fields_onload();	
+function generate_fields_onload(newField){
+	jQuery(newField).insertAfter('tr:last-child');	
+}	
+
+jQuery('.form-table').append('<a class="add button button-secondary" href="#">+ Add another word</a>');
 
 
 	//Add new fields
 	jQuery('.add').bind('click', function(e){
+		
 		e.stopPropagation;
 		addCopy();
 		//console.log('bling');
@@ -41,14 +68,20 @@ jQuery('.form-table').append('<a class="add" href="#">Add another word</a>');
 
 
 function addCopy(){
+
+	i++;
+	console.log(i);
+	console.log(newField);
 	//Get the current parent tr
 	var closestRow = jQuery('tr').last();
 	//Insert the new field after
 	jQuery(newField).insertAfter(closestRow);
-	console.log(closestRow);
+	generate_unique_integer(newField);
+
+
 }
 
 
 
-});
+}, function(){});
 
